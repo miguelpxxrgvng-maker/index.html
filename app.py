b326d4fc04704e0ce36103b7d1d3dee4923b8e56
miguelpxxrgvng-maker <miguelpxxrgvng@gmail.com>
2026-03-23 06:55:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -196,4 +196,5 @@ def obtener_comentarios(producto_id):
 
 # ===== Ejecutar app =====
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
